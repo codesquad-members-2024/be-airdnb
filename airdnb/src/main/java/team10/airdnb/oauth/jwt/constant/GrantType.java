48b94952aa1a8c6c6d23906fa06008c5b26bd219
choejieun <wldms5221@icloud.com>
2024-06-05 06:36:08 +0900

    feat: jwt 구현

---
+++ airdnb/src/main/java/team10/airdnb/oauth/jwt/constant/GrantType.java
@@ -0,0 +1,16 @@
+package team10.airdnb.oauth.jwt.constant;
+
+import lombok.Getter;
+
+@Getter
+public enum GrantType {
+
+    BEARER("Bearer");
+
+    GrantType(String type) {
+        this.type = type;
+    }
+
+    private String type;
+
+}
