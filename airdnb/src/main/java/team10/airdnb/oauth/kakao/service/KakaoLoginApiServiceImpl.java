df9c449e85a520c88e9ad8a9dd73bcb6b4c02ea6
choejieun <wldms5221@icloud.com>
2024-06-05 06:52:09 +0900

    feat: kakao 정보 가져오기

---
+++ airdnb/src/main/java/team10/airdnb/oauth/kakao/service/KakaoLoginApiServiceImpl.java
@@ -0,0 +1,39 @@
+package team10.airdnb.oauth.kakao.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+import team10.airdnb.Member.constant.MemberType;
+import team10.airdnb.oauth.jwt.constant.GrantType;
+import team10.airdnb.oauth.kakao.client.KakaoUserInfoClient;
+import team10.airdnb.oauth.kakao.dto.KakaoUserInfoResponseDto;
+import team10.airdnb.oauth.model.OAuthAttributes;
+import team10.airdnb.oauth.service.SocialLoginApiService;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class KakaoLoginApiServiceImpl implements SocialLoginApiService {
+
+    private final KakaoUserInfoClient kakaoUserInfoClient;
+    private final String CONTENT_TYPE = "application/x-www-form-urlencoded;charset=utf8";
+
+    @Override
+    public OAuthAttributes getUserInfo(String accessToken) { // kakao에서 받은 accessToken
+        KakaoUserInfoResponseDto kakaoUserInfoResponseDto = kakaoUserInfoClient.getKakaoUserInfo(CONTENT_TYPE,
+                GrantType.BEARER.getType() + " " + accessToken);
+        KakaoUserInfoResponseDto.KakaoAccount kakaoAccount = kakaoUserInfoResponseDto.getKakaoAccount();
+        String email = kakaoAccount.getEmail();
+
+        return OAuthAttributes.builder()
+                .email(!StringUtils.hasText(email) ? kakaoUserInfoResponseDto.getId() : email)
+                .name(kakaoAccount.getProfile().getNickname())
+                .profile(kakaoAccount.getProfile().getThumbnailImageUrl())
+                .memberType(MemberType.KAKAO)
+                .build();
+    }
+
+}
