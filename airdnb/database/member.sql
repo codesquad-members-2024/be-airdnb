23fff158d6d2a360100677a558193e9c7a16aff7
choejieun <wldms5221@icloud.com>
2024-06-05 14:56:59 +0900

    feat: member 테이블 DDL 구현

---
+++ airdnb/database/member.sql
@@ -0,0 +1,9 @@
+CREATE TABLE member (
+                        id VARCHAR(255) NOT NULL PRIMARY KEY,
+                        password VARCHAR(255),
+                        member_type VARCHAR(255),
+                        profile VARCHAR(255),
+                        email VARCHAR(255),
+                        member_name VARCHAR(255),
+);
+
