1b059953e5fdfc7644fa56823eaf1c1a758c51cf
sangchu <didi1484@gmail.com>
2024-06-06 17:06:05 +0900

    feat: 숙소 softDelete 구현

--- src/main/java/com/airdnb/global/NotFoundException.java
+++ src/main/java/com/airdnb/global/NotFoundException.java
@@ -6,4 +6,8 @@ public class NotFoundException extends BizException {
     public NotFoundException(String message) {
         super(message, HttpStatus.NOT_FOUND);
     }
+
+    public NotFoundException(String message, HttpStatus httpStatus, Throwable cause) {
+        super(message, httpStatus, cause);
+    }
 }
