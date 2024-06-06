542a95fca59ec6d55e6a381ff0d53227904a4cb7
hiidy <hiidy115@gmail.com>
2024-06-06 21:34:37 +0900

    feat: JwtUtil 클래스 구현
    - secretkey에 따른 토큰 생성 기능
    - 키를 주입할 때 환경변수로 받은 키를 byte배열로 바꾼 뒤 암호화

---
+++ src/main/java/com/airdnb/security/JwtUtil.java
@@ -0,0 +1,38 @@
+package com.airdnb.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+    private Key key;
+
+    @Value("${jwt.token-valid-time}")
+    private Long tokenValidTime;
+
+    @PostConstruct
+    protected void init() {
+        byte[] keyBytes = Base64.getDecoder().decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String createToken(String id) {
+        return Jwts.builder()
+            .subject(id)
+            .issuedAt(new Date())
+            .expiration(new Date(System.currentTimeMillis() + tokenValidTime))
+            .signWith(key)
+            .compact();
+
+    }
+}
