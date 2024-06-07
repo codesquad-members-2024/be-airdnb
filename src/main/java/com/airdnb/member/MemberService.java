69f04ebac9c77b4574984f9c2f41f04c5cbc8f04
hiidy <hiidy115@gmail.com>
2024-06-07 15:01:26 +0900

    refactor: transactional 어노테이션을 메서드 단위로 적용

--- src/main/java/com/airdnb/member/MemberService.java
+++ src/main/java/com/airdnb/member/MemberService.java
@@ -5,7 +5,6 @@ import com.airdnb.member.dto.MemberRegistration;
 import com.airdnb.member.dto.MemberVerification;
 import com.airdnb.member.entity.Member;
 import com.airdnb.security.JwtUtil;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -13,9 +12,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class MemberService {
 
@@ -25,21 +24,25 @@ public class MemberService {
     private final MemberMapper memberMapper;
     private final JwtUtil jwtUtil;
 
+    @Transactional(readOnly = true)
     public Member findMemberById(String id) {
         return memberRepository.findById(id)
             .orElseThrow(() -> new NotFoundException("id와 일치하는 유저가 존재하지 않습니다."));
     }
 
+    @Transactional
     public void saveMember(Member member) {
         memberRepository.save(member);
     }
 
+    @Transactional
     public void register(MemberRegistration memberRegistration) {
         Member member = memberMapper.toMember(memberRegistration);
         member.setPassword(passwordEncoder.encode(member.getPassword()));
         saveMember(member);
     }
 
+    @Transactional
     public String verify(MemberVerification memberVerification) {
         Authentication authentication = authenticationManager.authenticate(
             new UsernamePasswordAuthenticationToken(
