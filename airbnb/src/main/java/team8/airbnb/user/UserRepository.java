f58df898c54cb27b7a67337d8413eb51d8ed01d8
kalia <fmoon11200@naver.com>
2024-06-07 11:44:09 +0900

    feat: 로그인 로직 기능 구현

--- airbnb/src/main/java/team8/airbnb/user/UserRepository.java
+++ airbnb/src/main/java/team8/airbnb/user/UserRepository.java
@@ -1,7 +1,15 @@
 package team8.airbnb.user;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+  List<User> findByEmail(String email);
+
+  User findByPhoneNumber(String phoneNumber);
+
+  User findByUsername(String username);
 }
--- airbnb/src/main/java/team8/airbnb/user/UserService.java
+++ airbnb/src/main/java/team8/airbnb/user/UserService.java
@@ -1,7 +1,10 @@
 package team8.airbnb.user;
 
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @Service
@@ -13,8 +16,27 @@ public class UserService {
     this.userRepository = userRepository;
   }
 
-  public void save(User user) {
+  public User findByUsername(String username) {
+    return userRepository.findByUsername(username);
+  }
+
+  public Long save(User user) {
+    List<User> usersByEmail = userRepository.findByEmail(user.getEmail());
+    if (!usersByEmail.isEmpty()) {
+      throw new ResponseStatusException(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다.");
+    }
+
+    User existingPhoneNumber = userRepository.findByPhoneNumber(user.getPhoneNumber());
+    if (existingPhoneNumber != null) {
+      throw new ResponseStatusException(HttpStatus.CONFLICT, "이미 존재하는 전화번호입니다.");
+    }
+
+    User existingUser = userRepository.findByUsername(user.getUsername());
+    if (existingUser != null) {
+      throw new ResponseStatusException(HttpStatus.CONFLICT, "이미 존재하는 회원의 이름입니다.");
+    }
+
     User savedUser = userRepository.save(user);
-    log.debug("가입된 유저: {}", savedUser.getUsername());
+    return savedUser.getId();
   }
 }
