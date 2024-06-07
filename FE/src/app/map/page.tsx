36b61d0e935e01b6b06a81d5bbfe6ddde210b0b6
Miensoap <miensop51@gmail.com>
2024-06-07 22:50:02 +0900

    Next.js 삭제

--- FE/src/app/map/page.tsx
+++
@@ -1,9 +0,0 @@
-import App from "../../container/app";
-
-export default function Home() {
-  return (
-    <div>
-      <App />
-    </div>
-  );
-}
--- FE/src/components/login/types.ts
+++
@@ -1,5 +0,0 @@
-interface LoginResponse {
-  token: string;
-  userId: number; // 화면에 보여줄때만사용 절대이걸로요청 xx 토큰으로요청
-  userName: string;
-}
