a695d9048a1891ffe4e308535df2424d6dd3807c
kalia <fmoon11200@naver.com>
2024-06-07 11:43:07 +0900

    feat: login api 추가

--- airbnb/src/main/java/team8/airbnb/user/UserController.java
+++ airbnb/src/main/java/team8/airbnb/user/UserController.java
@@ -2,16 +2,32 @@ package team8.airbnb.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import team8.airbnb.user.jwtToken.JwtUtil;
+import team8.airbnb.user.loginDto.LoginRequest;
 
 @RestController
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class UserController {
 
   private final UserService userService;
+  private final JwtUtil jwtUtil;
+
+  @PostMapping("/login")
+  public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+    User foundUser = userService.findByUsername(loginRequest.username());
+    if (foundUser != null && foundUser.getPassword().equals(loginRequest.password())) {
+      String token = jwtUtil.generateToken(foundUser.getUsername());
+      return ResponseEntity.ok(token);
+    }
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("인증 실패");
+  }
 
   @ResponseStatus(HttpStatus.CREATED)
   @PostMapping("/registration")
