08f0985d355761ad925c2d57550421cf9ce8a03a
seondays <agseon9@gmail.com>
2024-06-07 18:37:47 +0900

    refactor: Amenity 필드타입 enum으로 변경

--- BE/airdnb/src/main/java/com/team01/airdnb/amenity/Amenity.java
+++ BE/airdnb/src/main/java/com/team01/airdnb/amenity/Amenity.java
@@ -2,6 +2,8 @@ package com.team01.airdnb.amenity;
 
 import com.team01.airdnb.accommadation.Accommodation;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -20,16 +22,24 @@ public class Amenity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-  private Boolean tv;
-  private Boolean kitchen;
+  @Enumerated(EnumType.STRING)
+  private AmenityStatus tv;
+  @Enumerated(EnumType.STRING)
+  private AmenityStatus kitchen;
   private Integer beds;
   private Integer bathrooms;
-  private Boolean wirelessInternet;
-  private Boolean washingMachine;
-  private Boolean freeParking;
-  private Boolean paidParking;
-  private Boolean airConditioner;
-  private Boolean dedicatedWorkspace;
+  @Enumerated(EnumType.STRING)
+  private AmenityStatus wirelessInternet;
+  @Enumerated(EnumType.STRING)
+  private AmenityStatus washingMachine;
+  @Enumerated(EnumType.STRING)
+  private AmenityStatus freeParking;
+  @Enumerated(EnumType.STRING)
+  private AmenityStatus paidParking;
+  @Enumerated(EnumType.STRING)
+  private AmenityStatus airConditioner;
+  @Enumerated(EnumType.STRING)
+  private AmenityStatus dedicatedWorkspace;
 
   @OneToOne
   @JoinColumn(name = "accommodation_id")
