e39aff8477837b990741c5ea33700e2944b179ca
Miensoap <miensop51@gmail.com>
2024-06-09 00:46:47 +0900

    Chore. SecurityConfig 정리

--- BE/src/main/java/team07/airbnb/config/security/SecurityConfig.java
+++ BE/src/main/java/team07/airbnb/config/security/SecurityConfig.java
@@ -1,22 +1,23 @@
 package team07.airbnb.config.security;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import team07.airbnb.domain.auth.JwtAuthenticationFilter;
 import team07.airbnb.domain.user.service.JwtAndOAuthUserService;
 
-import java.util.Collections;
 import java.util.List;
 
 
@@ -30,46 +31,51 @@ public class SecurityConfig {
     private final JwtOAuthSuccessHandler successHandler;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
+    private final String LOCALHOST_REACT = "http://localhost:3000";
+    private final String PROD_REACT = "https://squadbnb.site:3000";
+    private final String PROD_HTTP = "https://squadbnb.site:80";
+    private final String PROD_HTTPS = "https://squadbnb.site:443";
+
+    @Bean
+    public Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> authorizeRequests() {
+        return authorize -> authorize
+//                .requestMatchers("/review/**", "/payment/**", "/booking/**").authenticated()
+//                .anyRequest().authenticated()
+                .anyRequest().permitAll();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of(
+                PROD_REACT,
+                PROD_HTTP,
+                PROD_HTTPS,
+                LOCALHOST_REACT
+        ));
+        config.setAllowedMethods(List.of("POST", "GET", "PUT", "DELETE"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(
-                        AbstractHttpConfigurer::disable
-                )
-                .cors(corsCustomizer -> corsCustomizer.configurationSource(request -> {
-                    CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(List.of("http://localhost:3000", "https://squadbnb.site:443", "https://squadbnb.site:3000", "https://squadbnb.site:80"));
-                    config.setAllowedMethods(List.of("POST", "GET", "PUT", "DELETE"));
-                    config.setAllowCredentials(true);
-                    config.setAllowedHeaders(Collections.singletonList("*"));
-                    return config;
-                }))
-                .headers(
-                        (headerConfig) -> headerConfig.frameOptions(
-                                HeadersConfigurer.FrameOptionsConfig::disable
-                        )
-                )
-                .authorizeHttpRequests((authorizeRequest) -> authorizeRequest
-//                        .requestMatchers("/review/**", "/payment/**", "/booking/**").authenticated()
-//                        .anyRequest().authenticated()
-                            .anyRequest().permitAll()
-                )
-                .logout(
-                        (logoutConfig) -> logoutConfig.logoutSuccessUrl("/")
-                )
-                // OAuth2 로그인 기능에 대한 여러 설정
-                // oauth2 로그인 추가
-                .oauth2Login(
-                        oAuth -> {
-                            oAuth.userInfoEndpoint(userInfo -> userInfo.userService(oAuth2UserService));
-                            oAuth.successHandler(successHandler);
-                        }
-                )
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .authorizeHttpRequests(authorizeRequests())
+                .logout(logout -> logout.logoutSuccessUrl("/"))
+                .oauth2Login(oauth2 -> {
+                    oauth2.userInfoEndpoint(userInfo -> userInfo.userService(oAuth2UserService));
+                    oauth2.successHandler(successHandler);
+                })
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-
-        ;
 
         return http.build();
     }
 }
-
