237694e2dea52149c6956e975615ae34ad7a36a5
yonghyeonpark <pyh4616@naver.com>
2024-06-08 12:45:44 +0900

    feat: 컨트롤러 임시 구현

--- src/main/java/codesquad/team05/web/user/UserController.java
+++ src/main/java/codesquad/team05/web/user/UserController.java
@@ -1,4 +1,25 @@
 package codesquad.team05.web.user;
 
+import codesquad.team05.domain.user.User;
+import codesquad.team05.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    // Service 주입으로 변경 예정
+    private final UserRepository userRepository;
+
+    // Dto 사용으로 수정 예정
+    @PostMapping("/users")
+    public void join(@RequestBody User user) {
+        userRepository.save(user);
+    }
+
+    @GetMapping("/users/{loginId}")
+    public User userDetails(@PathVariable String loginId) {
+        return userRepository.findById(loginId).get(); // Optional 처리 예정
+    }
 }
