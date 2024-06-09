9a10619df12f53dda196ccb1fea3030d80c4aab2
Miensoap <miensop51@gmail.com>
2024-06-09 17:15:05 +0900

    Fix. 로그인 후 새로고침

--- FE/src/services/auth.js
+++ FE/src/services/auth.js
@@ -1,18 +1,42 @@
+import axios from "axios";
+
+export const handleOAuth = async (code, state, provider) => {
+  try {
+    const response = await axios.get(
+      `https://squadbnb.site/api/login/oauth2/code/${String(
+        provider.provider
+      )}?code=${code}&state=${state}`
+    );
+    const data = response.data; // 응답 데이터
+    localStorage.setItem("token", data.token);
+    localStorage.setItem("userId", data.userId);
+    localStorage.setItem("userName", data.userName);
+
+    alert("로그인 성공: " + data.userName + "님 환영합니다!");
+  } catch (error) {
+    alert("로그인 실패: " + error.response.data.message);
+  }
+  window.location.reload();
+  window.location.replace("/");
+};
+
 export function logout() {
   // 로컬 스토리지에서 사용자 정보 삭제
+  const userName = localStorage.getItem("userName");
+
   localStorage.removeItem("token");
   localStorage.removeItem("userName");
   localStorage.removeItem("userId");
+
+  alert(userName + "님 안녕히가세요!");
 
   // 서버에 로그아웃 요청 보내기
   fetch("https://squadbnb.site/api/logout", {
     method: "POST",
   }).then((response) => {
     if (response.ok) {
-      // 로그아웃 성공 시 추가 작업 (필요한 경우)
       console.log("Logged out successfully");
     } else {
-      // 로그아웃 실패 시 처리 (필요한 경우)
       console.error("Error during logout");
     }
     window.location.reload();
--- FE/src/services/redirect.jsx
+++ FE/src/services/redirect.jsx
@@ -1,30 +1,9 @@
-import axios from "axios";
 import { useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { handleOAuth } from "./auth";
 
 const RedirectPage = (provider) => {
   const location = useLocation();
-  const navigate = useNavigate();
-
-  const handleOAuth = async (code, state) => {
-    try {
-      const response = await axios.get(
-        `https://squadbnb.site/api/login/oauth2/code/${String(
-          provider.provider
-        )}?code=${code}&state=${state}`
-      );
-      const data = response.data; // 응답 데이터
-      console.log("로그인 성공: " + data);
-      localStorage.setItem("token", data.token);
-      localStorage.setItem("userId", data.userId);
-      localStorage.setItem("userName", data.userName);
-
-      alert("로그인 성공: " + data.userName + "님 환영합니다!");
-    } catch (error) {
-      alert("로그인 실패: " + error.response.data.message);
-    }
-    navigate("/");
-  };
 
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
@@ -32,9 +11,9 @@ const RedirectPage = (provider) => {
     const state = searchParams.get("state");
 
     if (code) {
-      handleOAuth(code, state);
+      handleOAuth(code, state, provider);
     }
-  }, [location]);
+  }, [location, provider]);
 
   return (
     <div
