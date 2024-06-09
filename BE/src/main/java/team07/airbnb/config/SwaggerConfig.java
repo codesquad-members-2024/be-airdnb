58b273e1751bbe33dca14651145414c609b8dce0
Miensoap <miensop51@gmail.com>
2024-06-09 14:17:19 +0900

    Chore. application.yml , @Nullable
    .
    
    d

--- BE/src/main/java/team07/airbnb/config/SwaggerConfig.java
+++ BE/src/main/java/team07/airbnb/config/SwaggerConfig.java
@@ -2,15 +2,22 @@ package team07.airbnb.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl("https://squadbnb/api");
+
         return new OpenAPI()
+                .servers(List.of(server))
                 .info(new Info()
                         .title("Squadbnb API")
                         .version("1.0")
--- BE/src/main/java/team07/airbnb/config/WebConfig.java
+++ BE/src/main/java/team07/airbnb/config/WebConfig.java
@@ -1,6 +1,8 @@
 package team07.airbnb.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import team07.airbnb.util.UserEntityArgumentResolver;
@@ -19,5 +21,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(userEntityArgumentResolver);
+    }
+
+    @Bean
+    ForwardedHeaderFilter forwardedHeaderFilter() {
+        return new ForwardedHeaderFilter();
     }
 }
