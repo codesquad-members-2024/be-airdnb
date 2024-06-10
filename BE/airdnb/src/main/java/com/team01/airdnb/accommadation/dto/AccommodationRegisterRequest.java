cad0878cf83937f8fb5bdf52d58688a3b99a3927
seondays <agseon9@gmail.com>
2024-06-10 17:02:06 +0900

    feat: 숙소 등록을 위한 request dto 구현 #3

---
+++ BE/airdnb/src/main/java/com/team01/airdnb/accommadation/dto/AccommodationRegisterRequest.java
@@ -0,0 +1,68 @@
+package com.team01.airdnb.accommadation.dto;
+
+import com.team01.airdnb.accommadation.Accommodation;
+import com.team01.airdnb.amenity.Amenity;
+import com.team01.airdnb.amenity.AmenityStatus;
+import com.team01.airdnb.image.Image;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public record AccommodationRegisterRequest(
+    String title,
+    String content,
+    Long price,
+    Integer discount,
+    Integer maxAdults,
+    Integer maxChildren,
+    Integer maxInfants,
+    Integer maxPets,
+    String address,
+    List<String> images,
+    AmenityStatus tv,
+    AmenityStatus kitchen,
+    AmenityStatus washing_machine,
+    AmenityStatus free_parking,
+    AmenityStatus paid_parking,
+    AmenityStatus air_conditioning,
+    AmenityStatus dedicated_workspace,
+    Integer beds,
+    Integer bathrooms,
+    String userId
+) {
+
+  public Accommodation toAccommodationEntity() {
+    return Accommodation.builder()
+        .title(title)
+        .content(content)
+        .price(price)
+        .discount(discount)
+        .maxAdults(maxAdults)
+        .maxChildren(maxChildren)
+        .maxInfants(maxInfants)
+        .maxPets(maxPets)
+        .address(address)
+        .build();
+  }
+
+  public List<Image> toImageEntity() {
+    return images.stream()
+        .map(imagePath -> Image.builder()
+            .imagePath(imagePath)
+            .build())
+        .collect(Collectors.toList());
+  }
+
+  public Amenity toAmenityEntity() {
+    return Amenity.builder()
+        .tv(tv)
+        .kitchen(kitchen)
+        .washingMachine(washing_machine)
+        .freeParking(free_parking)
+        .paidParking(paid_parking)
+        .airConditioner(air_conditioning)
+        .dedicatedWorkspace(dedicated_workspace)
+        .beds(beds)
+        .bathrooms(bathrooms)
+        .build();
+  }
+}
