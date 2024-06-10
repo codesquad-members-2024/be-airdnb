2d38f56a5c8a029cd7fc17eb38c04abc569b99ff
mir <ogt124321@gmail.com>
2024-06-10 13:54:14 +0900

    refactor: 어노케이션 추가 + 테스트 변경

--- BE/airdnb/src/main/java/com/team01/airdnb/user/UserService.java
+++ BE/airdnb/src/main/java/com/team01/airdnb/user/UserService.java
@@ -1,5 +1,8 @@
 package com.team01.airdnb.user;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
 
 }
--- BE/airdnb/src/test/java/com/team01/airdnb/user/UserRepositoryTest.java
+++ BE/airdnb/src/test/java/com/team01/airdnb/user/UserRepositoryTest.java
@@ -1,30 +1,34 @@
 package com.team01.airdnb.user;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
 public class UserRepositoryTest {
-  @Autowired UserRepository userRepository;
+  @Autowired
+  UserRepository userRepository;
 
   @Test
-  @DisplayName("create users table test")
-  @Rollback(value = false)
-  void createUsersTable() {
-    //given
+  @DisplayName("유저 10명 생성 테스트")
+  void createUsers() {
     User user = new User();
-    user.setId("testId");
+    user.setId("user");
+    user.setUsername("username");
+    user.setPassword("password");
+    user.setRole(Role.CLIENT);
+    user.setAge(20);
+    userRepository.save(user);
 
-    //when
-    User savedUser = userRepository.save(user);
-
-    //then
-    assertThat(savedUser.getId()).isEqualTo(user.getId());
+    assertThat(user.getId()).isEqualTo("user");
+    assertThat(user.getUsername()).isEqualTo("username");
+    assertThat(user.getPassword()).isEqualTo("password");
+    assertThat(user.getRole()).isEqualTo(Role.CLIENT);
+    assertThat(user.getAge()).isEqualTo(20 );
   }
 }
