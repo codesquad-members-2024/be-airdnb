6dbcae615631db8c82e3f92d5b1be0d80814c242
seondays <agseon9@gmail.com>
2024-06-10 17:11:37 +0900

    feat: 호스트 값을 가져오기 위한 내용 구현 #3

--- BE/airdnb/src/main/java/com/team01/airdnb/user/UserService.java
+++ BE/airdnb/src/main/java/com/team01/airdnb/user/UserService.java
@@ -4,5 +4,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+  UserRepository userRepository;
 
+  public UserService(UserRepository userRepository){
+    this. userRepository = userRepository;
+  }
+
+  public User FindUserById(String userId){
+    return userRepository.findById(userId).orElseThrow();
+  }
 }
