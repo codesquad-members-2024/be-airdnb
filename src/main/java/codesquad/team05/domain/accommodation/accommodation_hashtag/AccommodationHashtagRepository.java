852c0ee667d070ea08b31f646748d77152537dc9
yonghyeonpark <pyh4616@naver.com>
2024-06-11 17:52:37 +0900

    feat: Accommodation과 Hashtag의 연관관계 중간 테이블 도메인 구현

---
+++ src/main/java/codesquad/team05/domain/accommodation/accommodation_hashtag/AccommodationHashtagRepository.java
@@ -0,0 +1,6 @@
+package codesquad.team05.domain.accommodation.accommodation_hashtag;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccommodationHashtagRepository extends JpaRepository<AccommodationHashtag, Long> {
+}
