2f740aaf0389abe1614d8d2b1519bdffb923ed88
seondays <agseon9@gmail.com>
2024-06-11 18:19:42 +0900

    feat: 숙소 상세보기를 위한 dto 구현 #3

---
+++ BE/airdnb/src/main/java/com/team01/airdnb/accommadation/dto/AccommodationDetailResponse.java
@@ -0,0 +1,31 @@
+package com.team01.airdnb.accommadation.dto;
+
+import com.team01.airdnb.amenity.dto.AmenityShowResponse;
+import com.team01.airdnb.comment.dto.CommentListResponse;
+import com.team01.airdnb.host.dto.HostSimplificationResponse;
+import com.team01.airdnb.image.dto.ImageListResponse;
+import java.util.List;
+import lombok.Builder;
+
+@Builder
+public record AccommodationDetailResponse(
+    String title,
+    String content,
+    Long price,
+    Integer discount,
+    String address,
+    Double latitude,
+    Double longitude,
+    Integer commentsNum,
+    Integer maxAdults,
+    Integer maxChildren,
+    Integer maxInfants,
+    Integer maxPets,
+    List<ImageListResponse> images,
+    List<CommentListResponse> comment,
+    HostSimplificationResponse host,
+    AmenityShowResponse amenity,
+    Boolean wishListFlag
+) {
+
+}
