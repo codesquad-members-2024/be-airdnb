12fed551c121f573618306f0c4b8fc4de9ebb637
growth-mango <esudatcom@gmail.com>
2024-06-11 19:12:07 +0900

    refactor: 불필요한 import 제외

--- be/src/main/java/com/yourbnb/member/model/dto/MemberResponse.java
+++ be/src/main/java/com/yourbnb/member/model/dto/MemberResponse.java
@@ -1,13 +1,7 @@
 package com.yourbnb.member.model.dto;
 
-import com.yourbnb.accommodation.model.Accommodation;
 import com.yourbnb.member.model.Member;
-import com.yourbnb.reservation.model.Reservation;
-import com.yourbnb.review.model.Review;
 import lombok.Getter;
-
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Getter
 public class MemberResponse {
