50662ee955ece8b4b846be5de38e6f9c6de9c2a3
seondays <agseon9@gmail.com>
2024-06-11 18:22:53 +0900

    feat: 어메니티와 호스트 응답을 위한 dto 구현

---
+++ BE/airdnb/src/main/java/com/team01/airdnb/amenity/dto/AmenityShowResponse.java
@@ -0,0 +1,5 @@
+package com.team01.airdnb.amenity.dto;
+
+public record AmenityShowResponse() {
+
+}
---
+++ BE/airdnb/src/main/java/com/team01/airdnb/host/dto/HostSimplificationResponse.java
@@ -0,0 +1,8 @@
+package com.team01.airdnb.host.dto;
+
+public record HostSimplificationResponse(
+    String hostId,
+    String rating
+) {
+
+}
