8d975cf66163811fec118fb3d91d1ede47087627
growth-mango <esudatcom@gmail.com>
2024-06-12 18:42:34 +0900

    feat: ReviewRepository 구현

---
+++ be/src/main/java/com/yourbnb/review/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.yourbnb.review.repository;
+
+import com.yourbnb.review.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
