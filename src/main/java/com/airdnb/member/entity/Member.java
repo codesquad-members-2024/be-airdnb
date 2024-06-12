11c24b3175e9bcab0c1be265d1694f41beef9756
hiidy <hiidy115@gmail.com>
2024-06-13 02:32:26 +0900

    feat: 로그인시 응답 데이터를 보내도록 수정

--- src/main/java/com/airdnb/member/entity/Member.java
+++ src/main/java/com/airdnb/member/entity/Member.java
@@ -1,5 +1,6 @@
 package com.airdnb.member.entity;
 
+import com.airdnb.chat.entity.MemberChatRoom;
 import com.airdnb.image.entity.Image;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -8,38 +9,46 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@SQLDelete(sql = "UPDATE member SET status = 'DELETED' WHERE id = ?")
+@SQLRestriction("status = 'ACTIVE'")
 public class Member {
 
     @Id
     private String id;
     @Setter
     private String password;
-
     private String name;
-
     private String role;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "img_id")
     private Image image;
     @Enumerated(EnumType.STRING)
     private MemberStatus status;
 
+    @OneToMany(mappedBy = "member")
+    private List<MemberChatRoom> memberChatRooms = new ArrayList<>();
+
     @Builder
-    public Member(String id, String password, String name, String role) {
+    public Member(String id, String password, String name, String role, List<MemberChatRoom> memberChatRooms) {
         this.id = id;
         this.password = password;
         this.name = name;
         this.role = role;
+        this.memberChatRooms = memberChatRooms;
         this.status = MemberStatus.ACTIVE;
     }
 
