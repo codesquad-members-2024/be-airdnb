20982009b802e5a159150fc3989696a084d9f298
Zoonmy <pjm2571@naver.com>
2024-06-12 13:57:41 +0900

    feat: reservation CREATE 기능 구현

--- airdnb/src/main/java/team10/airdnb/accommodation/exception/AccommodationIdNotFoundException.java
+++ airdnb/src/main/java/team10/airdnb/accommodation/exception/AccommodationIdNotFoundException.java
@@ -3,8 +3,9 @@ package team10.airdnb.accommodation.exception;
 import team10.airdnb.error.ErrorCode;
 import team10.airdnb.exception.BusinessException;
 
-public class AccommodationNotFoundException extends BusinessException {
-    public AccommodationNotFoundException() {
+public class AccommodationIdNotFoundException extends BusinessException {
+    public AccommodationIdNotFoundException() {
         super(ErrorCode.ACCOMMODATION_NOT_EXISTS);
     }
+
 }
---
+++ airdnb/src/main/java/team10/airdnb/reservation/controller/ReservationRestController.java
@@ -0,0 +1,33 @@
+package team10.airdnb.reservation.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import team10.airdnb.reservation.controller.request.ReservationCreateRequest;
+import team10.airdnb.reservation.entity.Reservation;
+import team10.airdnb.reservation.service.ReservationService;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class ReservationRestController {
+
+    private final ReservationService reservationService;
+
+    @PostMapping("/api/reservation")
+    public ResponseEntity<?> createReservation(@RequestBody @Valid ReservationCreateRequest request) {
+
+        Reservation savedReservation = reservationService.createReservation(request);
+
+        log.info("예약 생성 완료 : # {} : 숙소 이름 : {}, 예약자 이름 : {}",
+                savedReservation.getId(),
+                savedReservation.getAccommodation().getName(),
+                savedReservation.getMember().getMemberName());
+
+        return ResponseEntity.ok(savedReservation);
+    }
+}
