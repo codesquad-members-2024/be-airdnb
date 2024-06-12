ca582e25079b376e2f870162eff28dde40f51a0b
Zoonmy <pjm2571@naver.com>
2024-06-12 17:06:58 +0900

    feat: Reservation 생성/삭제 기능 구현, Response Dto 생성

--- airdnb/src/main/java/team10/airdnb/reservation/controller/ReservationRestController.java
+++ airdnb/src/main/java/team10/airdnb/reservation/controller/ReservationRestController.java
@@ -4,15 +4,15 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import team10.airdnb.reservation.controller.request.ReservationCreateRequest;
+import team10.airdnb.reservation.controller.response.ReservationSummaryResponse;
 import team10.airdnb.reservation.controller.response.ReservationInformationResponse;
-import team10.airdnb.reservation.entity.Reservation;
 import team10.airdnb.reservation.service.ReservationService;
 
 @Slf4j
@@ -23,23 +23,36 @@ public class ReservationRestController {
     private final ReservationService reservationService;
 
     @GetMapping("/api/reservation/{reservationId}")
-    public ResponseEntity<?> getReservation(@PathVariable long reservationId) {
+    public ResponseEntity<ReservationInformationResponse> getReservation(@PathVariable long reservationId) {
         ReservationInformationResponse response = reservationService.getReservation(reservationId);
 
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/api/reservation")
-    public ResponseEntity<?> createReservation(@RequestBody @Valid ReservationCreateRequest request) {
-
-        Reservation savedReservation = reservationService.createReservation(request);
+    public ResponseEntity<ReservationSummaryResponse> createReservation(@RequestBody @Valid ReservationCreateRequest request) {
+        ReservationSummaryResponse response = reservationService.createReservation(request);
 
         log.info("예약 생성 완료 : # {} : 숙소 이름 : {}, 예약자 이름 : {}",
-                savedReservation.getId(),
-                savedReservation.getAccommodation().getName(),
-                savedReservation.getMember().getMemberName());
+                response.reservationId(),
+                response.accommodationInformation().accommodationName(),
+                response.memberInformation().memberName()
+        );
 
-        return ResponseEntity.ok(savedReservation);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/api/reservation/{reservationId}")
+    public ResponseEntity<ReservationSummaryResponse> deleteReservation(@PathVariable long reservationId) {
+        ReservationSummaryResponse response = reservationService.deleteReservation(reservationId);
+
+        log.info("예약 삭제 완료 : # {} : 숙소 이름 : {}, 예약자 이름 : {}",
+                response.reservationId(),
+                response.accommodationInformation().accommodationName(),
+                response.memberInformation().memberName()
+        );
+
+        return ResponseEntity.ok(response);
     }
 
 }
--- airdnb/src/main/java/team10/airdnb/reservation/dto/AccommodationInformationDto.java
+++ airdnb/src/main/java/team10/airdnb/reservation/dto/AccommodationInformationDto.java
@@ -1,10 +1,16 @@
 package team10.airdnb.reservation.dto;
 
+import team10.airdnb.accommodation.entity.Accommodation;
+
 public record AccommodationInformationDto(
         long accommodationId,
         String accommodationName
 ) {
-    public static AccommodationInformationDto from(long accommodationId, String accommodationName) {
-        return new AccommodationInformationDto(accommodationId, accommodationName);
+    public static AccommodationInformationDto from(Accommodation accommodation) {
+        return new AccommodationInformationDto(
+                accommodation.getId(),
+                accommodation.getName()
+        );
     }
+    
 }
