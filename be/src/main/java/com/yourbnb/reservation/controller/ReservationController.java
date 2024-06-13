4612e9df121a596666ed510651f911a825110eff
growth-mango <esudatcom@gmail.com>
2024-06-13 18:23:51 +0900

    feat: 예약 생성을 위한 로직 구현 (controller)

---
+++ be/src/main/java/com/yourbnb/reservation/controller/ReservationController.java
@@ -0,0 +1,28 @@
+package com.yourbnb.reservation.controller;
+
+import com.yourbnb.reservation.model.Reservation;
+import com.yourbnb.reservation.model.dto.ReservationCreationRequest;
+import com.yourbnb.reservation.model.dto.ReservationCreationResponse;
+import com.yourbnb.reservation.service.ReservationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/reservation")
+public class ReservationController {
+    private final ReservationService reservationService;
+
+    @PostMapping
+    public ResponseEntity<ReservationCreationResponse> createReservation(@RequestBody ReservationCreationRequest request) {
+        Reservation createdReservation = reservationService.createReservation(request);
+
+        ReservationCreationResponse response = ReservationCreationResponse.from(createdReservation);
+
+        return ResponseEntity.ok(response);
+    }
+}
