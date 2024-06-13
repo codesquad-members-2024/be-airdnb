31792354546e09a8207286d17490244833b6a9d9
choejieun <wldms5221@icloud.com>
2024-06-13 20:42:27 +0900

    refactor: Request 수정

--- airdnb/src/main/java/team10/airdnb/accommodation/controller/request/AccommodationCreateRequest.java
+++ airdnb/src/main/java/team10/airdnb/accommodation/controller/request/AccommodationCreateRequest.java
@@ -4,9 +4,9 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import team10.airdnb.accommodation.entity.Accommodation;
-import team10.airdnb.accommodation_amenity.entity.AccommodationAmenity;
+import team10.airdnb.accommodation.entity.embedded.AccommodationFee;
+import team10.airdnb.accommodation.entity.embedded.RoomInfo;
 import team10.airdnb.accommodation_room_type.entity.AccommodationRoomType;
 import team10.airdnb.accommodation_type.entity.AccommodationType;
 
@@ -20,24 +20,27 @@ public record AccommodationCreateRequest(
         @NotBlank
         String name,
 
-        @Min(value = 1) @Max(value = 16)
+        @Min(1) @Max(16)
         long maxCapacity,
 
         Long accommodationType,
 
         Long accommodationRoomType,
 
-        @Min(value = 0) @Max(value = 50)
-        long bedroomCount,
+        @Min(0) @Max(50)
+        int bedroomCount,
 
-        @Min(value = 1) @Max(value = 50)
-        long bathroomCount,
+        @Min(1) @Max(50)
+        int bathroomCount,
 
-        @Min(value = 1) @Max(value = 50)
-        long bedCount,
+        @Min(1) @Max(50)
+        int bedCount,
 
-        @Min(value = 10000) @Max(value = 15000000)
-        long perPrice,
+        @Min(10000) @Max(15000000)
+        int dayRate,
+
+        @Min(0) @Max(1000000)
+        int cleaningFee,
 
         List<Long> amenityIds
 ) {
@@ -48,10 +51,15 @@ public record AccommodationCreateRequest(
                 .maxCapacity(maxCapacity)
                 .accommodationType(accommodationType)
                 .accommodationRoomType(accommodationRoomType)
-                .bedroomCount(bedroomCount)
-                .bathroomCount(bathroomCount)
-                .bedCount(bedCount)
-                .perPrice(perPrice)
+                .roomInfo(RoomInfo.builder()
+                        .bedroomCount(bedroomCount)
+                        .bathroomCount(bathroomCount)
+                        .bedCount(bedCount)
+                        .build())
+                .accommodationFee(AccommodationFee.builder()
+                        .dayRate(dayRate)
+                        .cleaningFee(cleaningFee)
+                        .build())
                 .build();
     }
 }
