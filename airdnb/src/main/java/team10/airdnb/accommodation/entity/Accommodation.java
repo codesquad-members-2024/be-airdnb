3976b01b735440099ae6fced75d89318150566e5
Zoonmy <pjm2571@naver.com>
2024-06-13 19:55:31 +0900

    feat: Accommodation 전체 조회, 개별 조회 기능 추가 / `@EntityGraph`를 활용한 fetch-join 구현

--- airdnb/src/main/java/team10/airdnb/accommodation/entity/Accommodation.java
+++ airdnb/src/main/java/team10/airdnb/accommodation/entity/Accommodation.java
@@ -3,6 +3,7 @@ package team10.airdnb.accommodation.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -60,7 +61,7 @@ public class Accommodation {
     @Override
     public String toString() {
         return String.format("""
-                        
+                                                
                         {
                         memberId='%s'
                         name='%s'
--- airdnb/src/main/java/team10/airdnb/accommodation/repository/AccommodationRepository.java
+++ airdnb/src/main/java/team10/airdnb/accommodation/repository/AccommodationRepository.java
@@ -1,7 +1,13 @@
 package team10.airdnb.accommodation.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team10.airdnb.accommodation.entity.Accommodation;
 
+import java.util.List;
+
 public interface AccommodationRepository extends JpaRepository<Accommodation, Long> {
+
+    @EntityGraph(attributePaths = {"accommodationType", "accommodationRoomType"})
+    List<Accommodation> findAll();
 }
