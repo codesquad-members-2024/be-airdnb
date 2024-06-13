4d2158d377ffda613d52f4365ca6bfabc7586abe
shimbaa <shim9597@gmail.com>
2024-06-13 14:38:15 +0900

    :recycle: refactor: 새로운 숙소 객체 생성 로직 이동
    기존 service 계층에서 request-Dto 로 이동 (toEntity())

--- airdnb/src/main/java/com/example/airdnb/service/accommodation/AccommodationService.java
+++ airdnb/src/main/java/com/example/airdnb/service/accommodation/AccommodationService.java
@@ -1,8 +1,6 @@
 package com.example.airdnb.service.accommodation;
 
 import com.example.airdnb.domain.accommodation.Accommodation;
-import com.example.airdnb.domain.accommodation.Address;
-import com.example.airdnb.domain.accommodation.Image;
 import com.example.airdnb.domain.user.User;
 import com.example.airdnb.domain.user.User.Role;
 import com.example.airdnb.dto.accommodation.AccommodationCreationRequest;
@@ -11,9 +9,6 @@ import com.example.airdnb.repository.accommodation.AccommodationRepository;
 import com.example.airdnb.repository.user.UserRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.GeometryFactory;
-import org.locationtech.jts.geom.Point;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -33,47 +28,15 @@ public class AccommodationService {
     }
 
     public Accommodation createNewAccommodation(AccommodationCreationRequest request) {
+
+        // 호스트에 해당하는 사용자를 임시로 생성하는 코드
+        // 추후 변경 예정
         User saveUser = userRepository.save(new User("abc", "bde", "a", Role.HOST));
         User host = findUserById(saveUser.getId());
-        List<Image> images = getImagesFromRequest(request);
 
-        Address address = buildAddress(request);
-
-        Accommodation accommodation = Accommodation.builder()
-                .address(address)// 임시 코드
-                .user(host)
-                .name(request.name())
-                .pricePerNight(request.pricePerNight())
-                .description(request.description())
-                .maxGuests(request.maxGuests())
-                .images(images)
-                .build();
+        Accommodation accommodation = request.toEntityWithHost(host);
 
         return accommodationRepository.save(accommodation);
-    }
-
-    private Address buildAddress(AccommodationCreationRequest request) {
-        //todo 주소 값들로 부터 좌표 값을 구하는 기능 추가 예정
-        GeometryFactory gf = new GeometryFactory();
-        Point point = gf.createPoint(
-                new Coordinate(
-                        request.address().latitude(),
-                        request.address().longitude()
-                )); // 임시 코드
-
-        return new Address(
-                request.address().country(),
-                request.address().state(),
-                request.address().city(),
-                request.address().detail(),
-                point
-        );
-    }
-
-    private List<Image> getImagesFromRequest(AccommodationCreationRequest request) {
-        return request.imageUrls().stream()
-                .map(Image::new)
-                .toList();
     }
 
     private User findUserById(Long id) {
