2745dc9862df8faccbee071a4ea5a091129c8950
mir <ogt124321@gmail.com>
2024-06-13 11:28:15 +0900

    feat: 코멘트에 필요한 유저의 이름과 호스트의 이름 평점을 반환하는 기능 구현 #4

--- BE/airdnb/src/main/java/com/team01/airdnb/user/UserService.java
+++ BE/airdnb/src/main/java/com/team01/airdnb/user/UserService.java
@@ -1,16 +1,35 @@
 package com.team01.airdnb.user;
 
+import com.team01.airdnb.comment.CommentRepository;
+import com.team01.airdnb.user.dto.UserCommentResponse;
+import com.team01.airdnb.user.dto.UserHostResponse;
+import java.util.NoSuchElementException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
   UserRepository userRepository;
+  CommentRepository commentRepository;
 
-  public UserService(UserRepository userRepository){
+  public UserService(UserRepository userRepository, CommentRepository commentRepository){
     this. userRepository = userRepository;
+    this.commentRepository = commentRepository;
+
   }
 
   public User FindUserById(String userId){
-    return userRepository.findById(userId).orElseThrow();
+    return userRepository.findById(userId)
+        .orElseThrow(() -> new NoSuchElementException("해당하는 유저가 존재하지 않습니다"));
+  }
+
+  public UserHostResponse getHostResponse(User user){
+    return UserHostResponse.builder()
+        .username(user.username)
+        .score(commentRepository.findAverageScoreByUser(user))
+        .build();
+  }
+
+  public UserCommentResponse getCommentResponse(User user){
+    return UserCommentResponse.builder().username(user.getUsername()).build();
   }
 }
