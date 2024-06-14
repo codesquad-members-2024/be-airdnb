b49e4f91315cd829ff146818ed6e90d4ecb80555
growth-mango <esudatcom@gmail.com>
2024-06-14 15:50:12 +0900

    feat: reservationId, accomodationId, memberId 에 따른 예약 조회하기 위한 메서드 구현 (repository)

--- be/src/main/java/com/yourbnb/reservation/repository/ReservationRepository.java
+++ be/src/main/java/com/yourbnb/reservation/repository/ReservationRepository.java
@@ -1,7 +1,14 @@
 package com.yourbnb.reservation.repository;
 
 import com.yourbnb.reservation.model.Reservation;
+import com.yourbnb.review.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    Optional<Reservation> findById(Long reservationId);
+    List<Reservation> findByAccommodationId(Long accommodationId);
+    List<Reservation> findByMemberId(String memberId);
 }
