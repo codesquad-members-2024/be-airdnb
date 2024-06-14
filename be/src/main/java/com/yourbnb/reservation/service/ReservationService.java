be4b779264a9c97dfc8a30e0f83b5e1e4b978885
growth-mango <esudatcom@gmail.com>
2024-06-14 15:50:48 +0900

    feat: reservationId, accomodationId, memberId 에 따른 예약 조회하기 위한 서비스 로직 구현 (service)

--- be/src/main/java/com/yourbnb/reservation/service/ReservationService.java
+++ be/src/main/java/com/yourbnb/reservation/service/ReservationService.java
@@ -9,8 +9,11 @@ import com.yourbnb.reservation.model.dto.ReservationCreationRequest;
 import com.yourbnb.reservation.repository.ReservationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +25,7 @@ public class ReservationService {
     private final MemberRepository memberRepository;
     private final AccommodationRepository accommodationRepository;
 
+    @Transactional
     public Reservation createReservation(ReservationCreationRequest reservationCreationRequest) {
         Member member = memberRepository.findById(reservationCreationRequest.memberId())
                 .orElseThrow(() -> new RuntimeException("Member not found"));
@@ -36,5 +40,17 @@ public class ReservationService {
         Reservation reservation = reservationCreationRequest.toEntity(member, accommodation, totalPrice);
 
         return reservationRepository.save(reservation);
+    }
+
+    public Optional<Reservation> getReservationById(Long reservationId) {
+        return reservationRepository.findById(reservationId);
+    }
+
+    public List<Reservation> getReservationsByMemberId(String memberId) {
+        return reservationRepository.findByMemberId(memberId);
+    }
+
+    public List<Reservation> getReservationsByAccommodationId(Long accommodationId) {
+        return reservationRepository.findByAccommodationId(accommodationId);
     }
 }
