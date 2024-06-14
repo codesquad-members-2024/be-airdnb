5d5072765ca8d5252dc2a5e758addb2345f66998
horang <fnelclsrn123@gmail,com>
2024-06-14 14:01:30 +0900

    refactor: user리포지토리 id 타입 변경
    -String에서 Long으로 변경

--- src/main/java/codesquad/team05/domain/user/User.java
+++ src/main/java/codesquad/team05/domain/user/User.java
@@ -3,11 +3,11 @@ package codesquad.team05.domain.user;
 import codesquad.team05.domain.accommodation.Accommodation;
 import codesquad.team05.domain.accommodation.like.Like;
 import codesquad.team05.domain.accommodation.reservation.Reservation;
+import codesquad.team05.domain.accommodation.reservation.review.Review;
+import codesquad.team05.domain.host.Host;
+import codesquad.team05.web.dto.response.user.UserResponse;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 import java.time.LocalDate;
@@ -19,6 +19,10 @@ import java.util.List;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
     private String loginId;
     private String name;
     private String password;
@@ -26,15 +30,39 @@ public class User {
     private String address;
     private LocalDate birthDate;
 
-    @JsonIgnore
-    @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST)
-    private List<Reservation> reservations = new ArrayList<>();
-
-    @JsonIgnore
     @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST)
     private List<Accommodation> accommodations = new ArrayList<>();
 
-    @JsonIgnore
-    @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
-    private List<Like> likes = new ArrayList<>();
+    @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST)
+    List<Reservation> reservation = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST)
+    List<Review> reviews = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    List<Like> likes = new ArrayList<>();
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Host host;
+
+
+    public void enrollHost() {
+        if (this.host == null) {
+            Host host = new Host();
+            host.setUser(this);
+            this.host = host;
+        }
+
+
+    }
+
+    public UserResponse toEntity() {
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(id);
+        userResponse.setName(name);
+        userResponse.setLoginId(loginId);
+
+        return userResponse;
+    }
+
 }
--- src/main/java/codesquad/team05/domain/user/UserRepository.java
+++ src/main/java/codesquad/team05/domain/user/UserRepository.java
@@ -2,5 +2,5 @@ package codesquad.team05.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User,Long> {
 }
