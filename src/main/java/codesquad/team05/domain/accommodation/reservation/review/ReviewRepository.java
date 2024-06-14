2ea9b325c96e1628adeaf0dfea4af43b878b9e87
horang <fnelclsrn123@gmail,com>
2024-06-14 14:02:53 +0900

    feat: review 엔티티, 레포지토리 생성

---
+++ src/main/java/codesquad/team05/domain/accommodation/reservation/review/ReviewRepository.java
@@ -0,0 +1,6 @@
+package codesquad.team05.domain.accommodation.reservation.review;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
