11cb7cd99c4339d0190c1a863003fc806637e301
horang <fnelclsrn123@gmail,com>
2024-06-14 14:00:24 +0900

    feat: accommodation crud를 위한 기능 생성
    -curd를 위한 service, controller, 엔티티
    -update, create를 위한 Business 서비스와 조회를 위한 서비스 분리

---
+++ src/main/java/codesquad/team05/web/controller/ReservationController.java
@@ -0,0 +1,32 @@
+package codesquad.team05.web.controller;
+
+import codesquad.team05.service.reservation.ReservationService;
+import codesquad.team05.web.dto.request.reservation.ReservationSave;
+import codesquad.team05.web.dto.response.reservation.ReservationResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/reservations")
+@RequiredArgsConstructor
+@Slf4j
+public class ReservationController {
+
+    // to-do
+    // User 기능 완성하면 @AuthenticationPrincipal PrincipalDetails userDetails을 활용해서 세션으로 userID집어넣기
+    private final ReservationService reservationService;
+
+    @PostMapping("/{id}")
+    public void reserve(@PathVariable Long id, @RequestBody ReservationSave requestForm) {
+        reservationService.reservation(1L, id, requestForm.toServiceDto());
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ReservationResponse> get(@PathVariable Long id) {
+        ReservationResponse reservationResponse = reservationService.get(id);
+        return ResponseEntity.ok(reservationResponse);
+    }
+}
