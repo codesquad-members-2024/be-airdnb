dd4e9070cbe4f295b17ea9c75a522da3a761721a
kimjooyoung <kjy915875@gmail.com>
2024-06-16 16:25:37 +0900

    fix. Authentication AOP 권한 예외 메시지 수정

--- BE/src/main/java/team07/airbnb/common/auth/aop/AuthenticationAspect.java
+++ BE/src/main/java/team07/airbnb/common/auth/aop/AuthenticationAspect.java
@@ -2,6 +2,7 @@ package team07.airbnb.common.auth.aop;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.antlr.v4.runtime.Token;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -11,6 +12,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import team07.airbnb.common.auth.jwt.JwtAuthenticationFilter;
+import team07.airbnb.data.user.dto.response.TokenUserInfo;
 import team07.airbnb.data.user.enums.Role;
 import team07.airbnb.exception.auth.AuthenticateException;
 import team07.airbnb.exception.auth.UnAuthorizedException;
@@ -37,9 +39,11 @@ public class AuthenticationAspect {
         }
 
         if (!userHasGrant(authenticated, authentication)) {
-            throw new UnAuthorizedException("AOP 에서 권한 예외 발생함");
+            TokenUserInfo user = (TokenUserInfo) authentication.getPrincipal();
+            throw new UnAuthorizedException("사용자 {%d} 가 권한이 없는 서비스에 접근함. 필요권한 -> {%S}, 소유 권한 -> {%s}"
+                    .formatted(user.id(), authenticated.value().getKey(), user.stringRole()));
         }
-
+        
         return joinPoint.proceed();
     }
 
