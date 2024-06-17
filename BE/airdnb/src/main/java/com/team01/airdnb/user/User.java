861d74df98f625a0ebaf174a1a41ce434661ff5d
mir <ogt124321@gmail.com>
2024-06-17 11:24:47 +0900

    refactor: Oauth 적용을 위한 user entity 변경 #16

--- BE/airdnb/src/main/java/com/team01/airdnb/user/User.java
+++ BE/airdnb/src/main/java/com/team01/airdnb/user/User.java
@@ -5,48 +5,40 @@ import com.team01.airdnb.comment.Comment;
 import com.team01.airdnb.host.Host;
 import com.team01.airdnb.reservation.Reservation;
 import com.team01.airdnb.wishlist.Wishlist;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
-import jakarta.validation.constraints.Max;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import java.util.List;
-import lombok.Getter;
-import lombok.Setter;
+
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "users")
 public class User {
   @Id
-  @NotBlank(message = "아이디를 입력하세요")
-  @Size(min = 4, max = 20, message = "아이디는 4자 이상 20자 이하여야 합니다")
-  public String id;
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-  @NotBlank(message = "이름을 입력하세요")
-  @Size(min = 1, max = 10, message = "이름은 1자 이상 10자 이하여야 합니다")
-  public String username;
+  @NotBlank(message = "이름이 비어있습니다")
+  private String username;
 
-  @NotBlank(message = "비밀번호를 입력하세요")
-  @Size(min = 6, max = 20, message = "비밀번호는 6자 이상 20자 이하여야 합니다")
-  public String password;
+  @NotBlank(message = "이메일이 없습니다")
+  private String email;
+
+  @NotBlank(message = "프로필이 없습니다")
+  private String profile;
+
+  @NotBlank(message = "프로바이더가 비어있습니다")
+  private String provider;
 
   @Enumerated(value = EnumType.STRING)
-  public Role role;
+  private Role role;
 
-  @NotNull(message = "나이를 입력하세요")
-  @Max(value = 100, message = "나이는 100 이하여야 합니다")
-  public Integer age;
-
+  //연관 관계
   @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
   private Host host;
 
