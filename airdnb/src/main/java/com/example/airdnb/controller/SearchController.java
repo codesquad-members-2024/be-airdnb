1294af183a3feb8d82bc6773f8e291ae46b0410b
shimbaa <shim9597@gmail.com>
2024-06-17 16:45:30 +0900

    :recycle: refactor: 검색 조건 요청을 받는 컨트롤러 메서드 매개변수 변경
    기존 http body 로 받던 검색 조건 데이터를 쿼리 스트링을 통해 받는 방식으로 변경

--- airdnb/src/main/java/com/example/airdnb/controller/SearchController.java
+++ airdnb/src/main/java/com/example/airdnb/controller/SearchController.java
@@ -1,12 +1,15 @@
 package com.example.airdnb.controller;
 
+import com.example.airdnb.domain.accommodation.Accommodation;
 import com.example.airdnb.domain.search.SearchCondition;
 import com.example.airdnb.dto.search.SearchConditionRequest;
 import com.example.airdnb.service.SearchService;
+import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +21,15 @@ public class SearchController {
     private final SearchService searchService;
 
     @GetMapping
-    public void search(@RequestBody SearchConditionRequest searchConditionRequest) {
+    public void search(@ModelAttribute @Valid SearchConditionRequest searchConditionRequest) {
         SearchCondition searchCondition = searchConditionRequest.toEntity();
 
+        List<Accommodation> accommodations = searchService.searchWithConditions(searchCondition);
+
         log.info(searchCondition.toString());
+
+        for (Accommodation accommodation : accommodations) {
+            log.info(accommodation.toString());
+        }
     }
 }
--- airdnb/src/main/java/com/example/airdnb/dto/search/SearchConditionRequest.java
+++ airdnb/src/main/java/com/example/airdnb/dto/search/SearchConditionRequest.java
@@ -3,17 +3,26 @@ package com.example.airdnb.dto.search;
 import com.example.airdnb.domain.search.PriceRange;
 import com.example.airdnb.domain.search.SearchCondition;
 import com.example.airdnb.domain.search.StayPeriod;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
 import lombok.ToString;
 
 @ToString
 public class SearchConditionRequest {
+
+    @NotNull
     private final StayPeriod stayPeriod;
     private final PriceRange priceRange;
     private final Integer guestCount;
 
-    public SearchConditionRequest(StayPeriod stayPeriod, PriceRange priceRange, Integer guestCount) {
-        this.stayPeriod = stayPeriod;
-        this.priceRange = priceRange;
+    public SearchConditionRequest(
+            LocalDate checkInDate,
+            LocalDate checkOutDate,
+            Long minPrice,
+            Long maxPrice,
+            Integer guestCount) {
+        this.stayPeriod = StayPeriod.of(checkInDate, checkOutDate);
+        this.priceRange = PriceRange.of(minPrice, maxPrice);
         this.guestCount = guestCount;
     }
 
