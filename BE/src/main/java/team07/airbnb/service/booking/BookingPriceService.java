6cb8198701469f1b29d77a2b81d5e6443a23c750
kimjooyoung <kjy915875@gmail.com>
2024-06-18 13:39:25 +0900

    refactor. 무료 예약 버그 삭제

--- BE/src/main/java/team07/airbnb/service/booking/BookingPriceService.java
+++ BE/src/main/java/team07/airbnb/service/booking/BookingPriceService.java
@@ -4,9 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import team07.airbnb.data.booking.dto.PriceInfo;
 import team07.airbnb.data.booking.dto.transfer.BookingInfoForPriceInfo;
+import team07.airbnb.entity.ProductEntity;
+import team07.airbnb.service.accommodation.AccommodationService;
 import team07.airbnb.service.booking.price_policy.fee.AccommodationFee;
 import team07.airbnb.service.booking.price_policy.fee.ServiceFee;
 import team07.airbnb.service.discount.DiscountPolicyService;
+import team07.airbnb.service.product.ProductService;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
@@ -15,12 +18,14 @@ import java.time.temporal.ChronoUnit;
 @RequiredArgsConstructor
 public class BookingPriceService {
 
+    private final ProductService productService;
+
     private final DiscountPolicyService discountPolicyService;
     private final ServiceFee serviceFee;
     private final AccommodationFee accommodationFee;
 
     public PriceInfo getPriceInfo(BookingInfoForPriceInfo requestInfo) {
-        int roughTotalPrice = getRoughTotalPrice(requestInfo.avgPrice(), requestInfo.checkIn(), requestInfo.checkOut());
+        int roughTotalPrice = getRoughTotalPrice(requestInfo.accId(), requestInfo.checkIn(), requestInfo.checkOut(), requestInfo.headCount());
         int discountPrice = getDiscountPrice(roughTotalPrice);
         int serviceFee = getServiceFee(roughTotalPrice, discountPrice);
         int accommodationFee = getAccommodationFee(serviceFee);
@@ -33,9 +38,13 @@ public class BookingPriceService {
         );
     }
 
-    public int getRoughTotalPrice(int avgPrice, LocalDate checkIn, LocalDate checkOut) {
-        int days = (int) ChronoUnit.DAYS.between(checkIn, checkOut);
-        return avgPrice * days;
+    public int getRoughTotalPrice(Long accId, LocalDate checkIn, LocalDate checkOut, Integer headCount) {
+        int roughTotalPrice = productService.getInDateRangeOfAccommodation(accId, checkIn, checkOut, headCount)
+                .stream()
+                .mapToInt(ProductEntity::getPrice)
+                .sum();
+
+        return roughTotalPrice;
     }
 
     public int getDiscountPrice(int roughTotalPrice) {
