07c92e7d7ed6ef218b39472c729760107a204805
seondays <agseon9@gmail.com>
2024-06-18 21:59:12 +0900

    feat: 필터 종류를 받기 위한 dto 구현 #17

---
+++ BE/airdnb/src/main/java/com/team01/airdnb/accommadation/dto/AccommodationFilterRequest.java
@@ -0,0 +1,15 @@
+package com.team01.airdnb.accommadation.dto;
+
+import java.time.LocalDate;
+
+public record AccommodationFilterRequest(
+    LocalDate checkin,
+    LocalDate checkout,
+    Long minPrice,
+    Long maxPrice,
+    Integer adultCount,
+    Integer childrenCount,
+    Integer infantsCount
+) {
+
+}
