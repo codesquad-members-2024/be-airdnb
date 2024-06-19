c08a9f88b3979e03b19585edec4499f88bfdb1ab
pjm2571 <pjm2571@naver.com>
2024-06-19 13:42:43 +0900

    refactor: MiniSearchBar 중앙정렬되게 리팩토링

--- src/components/MiniSearchBar.svelte
+++ src/components/MiniSearchBar.svelte
@@ -28,34 +28,38 @@
   };
 </script>
 
-<div class="bg-white shadow-md rounded-full flex items-center w-full max-w-md p-2 border border-gray-300">
-  <button type="button" class="flex-grow px-4 py-2 border-r text-left" on:click={toggleDatePicker}>
-    <div class="text-sm text-gray-600">{formattedDates}</div>
-  </button>
-  <div class="flex-grow px-4 py-2 border-r">
-    <div class="text-sm text-gray-600">금액대 입력</div>
-  </div>
-  <div class="flex-grow px-4 py-2 border-r">
-    <div class="text-sm text-gray-600">인원 입력</div>
-  </div>
-  <div class="px-4 py-2">
-    <button class="bg-red-500 text-white rounded-full p-2 focus:outline-none">
-      <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" d="M12.9 14.32a8 8 0 111.414-1.414l4.294 4.294-1.414 1.414-4.294-4.294zM8 14a6 6 0 100-12 6 6 0 000 12z" clip-rule="evenodd"></path>
-      </svg>
+<div class="relative">
+  <div class="bg-white shadow-md rounded-full flex items-center w-full max-w-md p-2 border border-gray-300">
+    <button type="button" class="flex-grow px-4 py-2 border-r text-left" on:click={toggleDatePicker}>
+      <div class="text-sm text-gray-600">{formattedDates}</div>
     </button>
+    <div class="flex-grow px-4 py-2 border-r">
+      <div class="text-sm text-gray-600">금액대 입력</div>
+    </div>
+    <div class="flex-grow px-4 py-2 border-r">
+      <div class="text-sm text-gray-600">인원 입력</div>
+    </div>
+    <div class="px-4 py-2">
+      <button class="bg-red-500 text-white rounded-full p-2 focus:outline-none">
+        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" d="M12.9 14.32a8 8 0 111.414-1.414l4.294 4.294-1.414 1.414-4.294-4.294zM8 14a6 6 0 100-12 6 6 0 000 12z" clip-rule="evenodd"></path>
+        </svg>
+      </button>
+    </div>
   </div>
-</div>
 
-<DatePickerComponent
-  bind:checkIn={checkIn}
-  bind:checkOut={checkOut}
-  {dowLabels}
-  {monthLabels}
-  {onDatePickerPopup}
-  on:dateSelected={handleDateSelected}
-  on:toggle={toggleDatePicker}
-/>
+  {#if onDatePickerPopup}
+      <DatePickerComponent
+        bind:checkIn={checkIn}
+        bind:checkOut={checkOut}
+        {dowLabels}
+        {monthLabels}
+        {onDatePickerPopup}
+        on:dateSelected={handleDateSelected}
+        on:toggle={toggleDatePicker}
+      />
+  {/if}
+</div>
 
 <style>
   .border-r {
