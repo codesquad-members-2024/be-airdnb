20d1bf1c14f6400f518a940200b8f121deac934d
kimjooyoung <kjy915875@gmail.com>
2024-06-21 03:40:09 +0900

    booking controller request mapping 추가

--- BE/src/main/java/team07/airbnb/controller/booking/BookingGuestController.java
+++ BE/src/main/java/team07/airbnb/controller/booking/BookingGuestController.java
@@ -8,6 +8,7 @@ import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import team07.airbnb.data.booking.dto.PriceInfo;
@@ -24,6 +25,7 @@ import java.util.List;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
+@RequestMapping("/booking")
 @RequiredArgsConstructor
 public class BookingGuestController {
 
--- BE/src/main/java/team07/airbnb/controller/booking/BookingHostController.java
+++ BE/src/main/java/team07/airbnb/controller/booking/BookingHostController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import team07.airbnb.common.auth.aop.Authenticated;
@@ -25,6 +26,7 @@ import static org.springframework.http.HttpStatus.OK;
 import static team07.airbnb.data.user.enums.Role.HOST;
 
 @RestController
+@RequestMapping("/booking")
 @RequiredArgsConstructor
 public class BookingHostController {
 
