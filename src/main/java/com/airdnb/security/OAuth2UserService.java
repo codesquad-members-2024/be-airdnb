c4dc3c47b95c0c8c11a2d46295d88f2638301951
hiidy <hiidy115@gmail.com>
2024-06-21 03:41:46 +0900

    feat: Oauth2UserService 기능 구현
    - 로그인 성공시에 유저 정보를 저장
    - 이미 존재하는 유저인 경우는 저장하지 않음

---
+++ src/main/java/com/airdnb/security/OAuth2UserService.java
@@ -0,0 +1,42 @@
+package com.airdnb.security;
+
+import com.airdnb.member.MemberRepository;
+import com.airdnb.member.entity.Member;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class OAuth2UserService extends DefaultOAuth2UserService {
+
+    private final MemberRepository memberRepository;
+
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        Map<String, Object> attributes = super.loadUser(userRequest).getAttributes();
+
+        OAuthUserInfo oAuthUserInfo = OAuthUserInfo.ofGoogle(attributes);
+        
+        oauthVerify(oAuthUserInfo);
+
+        return super.loadUser(userRequest);
+    }
+
+    private void oauthVerify(OAuthUserInfo oAuthUserInfo) {
+        if (memberRepository.existsById(oAuthUserInfo.getEmail())) {
+            log.info("이미 존재하는 id입니다.");
+            return;
+        }
+        Member member = oAuthUserInfo.toEntity();
+        memberRepository.save(member);
+    }
+}
