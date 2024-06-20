3f64f39f51b3a9a9a0a9f99100beb67462e13ddd
kimjooyoung <kjy915875@gmail.com>
2024-06-21 03:33:07 +0900

    refactor. get 메서드 사용하는 부분 body가 아닌 model attribute로 변경

--- BE/src/main/java/team07/airbnb/controller/booking/BookingGuestController.java
+++ BE/src/main/java/team07/airbnb/controller/booking/BookingGuestController.java
@@ -4,7 +4,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +34,7 @@ public class BookingGuestController {
     @Operation(summary = "예약 요금 정보 조회", description = "예약의 요금 정보를 조회합니다.")
     @GetMapping
     @ResponseStatus(OK)
-    public PriceInfo getBookingPriceInfo(@RequestBody @Valid BookingRequest requestInfo) {
+    public PriceInfo getBookingPriceInfo(@ModelAttribute("requestInfo") @Valid BookingRequest requestInfo) {
         return bookingPriceService.getPriceInfo(
                 requestInfo.accommodationId(),
                 requestInfo.checkIn(),
