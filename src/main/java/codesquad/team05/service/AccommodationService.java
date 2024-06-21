ff2cd527fd141a20c6c1cc62bc501e7107fde1e0
yonghyeonpark <pyh4616@naver.com>
2024-06-21 19:40:30 +0900

    refactor: AccommodationService 수정
    - 파일 업로드 로직 수정
    - 파일 저장 로직 추가
    - 메서드 파라미터 인자 수정

--- src/main/java/codesquad/team05/service/AccommodationService.java
+++ src/main/java/codesquad/team05/service/AccommodationService.java
@@ -3,6 +3,7 @@ package codesquad.team05.service;
 import codesquad.team05.domain.accommodation.Accommodation;
 import codesquad.team05.domain.accommodation.AccommodationRepository;
 import codesquad.team05.domain.picture.Picture;
+import codesquad.team05.domain.picture.PictureRepository;
 import codesquad.team05.util.AccommodationMapper;
 import codesquad.team05.web.accommodation.dto.request.AccommodationSaveServiceRequest;
 import codesquad.team05.web.accommodation.dto.request.AccommodationUpdateServiceRequest;
@@ -25,6 +26,7 @@ public class AccommodationService {
 
     private final AmazonS3Client amazonS3Client;
     private final AccommodationRepository accommodationRepository;
+    private final PictureRepository pictureRepository;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
@@ -32,27 +34,17 @@ public class AccommodationService {
     @Value("${cloud.aws.s3.url}")
     private String s3Url;
 
-    public Long register(AccommodationSaveServiceRequest serviceRequest, List<String> pictures) {
-        Accommodation accommodation = AccommodationMapper.toEntity(serviceRequest);
-        List<Picture> picture = toPicture(pictures, accommodation);
-        accommodation.setPictures(picture);
-        return accommodationRepository.save(accommodation).getId();
-    }
-
-    public String uploadFile(MultipartFile file) throws IOException {
-        if (file == null || file.isEmpty()) {
-            return null;
+    public Long register(AccommodationSaveServiceRequest saveServiceRequest) {
+        Accommodation accommodation = AccommodationMapper.toEntity(saveServiceRequest);
+        Long accommodationId = accommodationRepository.save(accommodation).getId();
+        List<MultipartFile> files = saveServiceRequest.getFiles();
+        if (files != null && !files.isEmpty() && !files.stream().allMatch(MultipartFile::isEmpty)) {
+            List<String> pictureUrls = files.stream()
+                    .map(url -> uploadFileToS3(url, accommodationId))
+                    .toList();
+            savePicture(pictureUrls, accommodation);
         }
-        String filename = file.getOriginalFilename();
-        String fileUrl = s3Url + filename;
-
-        ObjectMetadata objectMetadata = new ObjectMetadata();
-        objectMetadata.setContentType(file.getContentType());
-        objectMetadata.setContentLength(file.getSize());
-
-        amazonS3Client.putObject(bucket, filename, file.getInputStream(), objectMetadata);
-
-        return fileUrl;
+        return accommodationId;
     }
 
     @Transactional(readOnly = true)
@@ -62,28 +54,47 @@ public class AccommodationService {
 
     public void updateAccommodation(
             Long accommodationId,
-            AccommodationUpdateServiceRequest newAccommodation,
-            List<String> url
+            AccommodationUpdateServiceRequest updateServiceRequest
     ) {
         Accommodation accommodation = accommodationRepository.findById(accommodationId).orElseThrow();
         accommodation.update(
-                newAccommodation.getName(),
-                newAccommodation.getPrice(),
-                newAccommodation.getAddress(),
-                newAccommodation.getMaxCapacity(),
-                newAccommodation.getRoomCount(),
-                newAccommodation.getBedCount(),
-                newAccommodation.getDescription(),
-                newAccommodation.getAmenity()
+                updateServiceRequest.getName(),
+                updateServiceRequest.getPrice(),
+                updateServiceRequest.getAddress(),
+                updateServiceRequest.getMaxCapacity(),
+                updateServiceRequest.getRoomCount(),
+                updateServiceRequest.getBedCount(),
+                updateServiceRequest.getDescription(),
+                updateServiceRequest.getAmenity()
         );
+        List<String> pictureUrls = updateServiceRequest.getFiles().stream()
+                .map(url -> uploadFileToS3(url, accommodationId))
+                .toList();
+        savePicture(pictureUrls, accommodation);
     }
 
-    private List<Picture> toPicture(List<String> pictures, Accommodation accommodation) {
-        return pictures.stream().map(url -> {
-            Picture picture = new Picture(url);
-            picture.setAccommodation(accommodation);
-            return picture;
-        }).toList();
+    private String uploadFileToS3(MultipartFile file, Long accommodationId) {
+        try {
+            String filename = file.getOriginalFilename();
+            String fileUrl = s3Url + accommodationId + "/" + filename;
+
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentType(file.getContentType());
+            objectMetadata.setContentLength(file.getSize());
+            amazonS3Client.putObject(bucket, filename, file.getInputStream(), objectMetadata);
+
+            return fileUrl;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void savePicture(List<String> pictures, Accommodation accommodation) {
+        pictures.forEach(url -> {
+            if (!pictureRepository.existsByUrl(url)) {
+                pictureRepository.save(new Picture(url, accommodation));
+            }
+        });
     }
 
     public void delete(Long id) {
