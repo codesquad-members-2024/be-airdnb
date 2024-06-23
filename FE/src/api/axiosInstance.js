bbdbb76a53610f2937741420e27942d2d291dc7f
Soap <miensop51@gmail.com>
2024-06-23 19:34:18 +0900

    Fix. 토큰 인터셉터로 추가하도록 변경
    인스턴스가 생성된 시점의 토큰이 유지되는 문제 해결

--- FE/src/api/axiosInstance.js
+++ FE/src/api/axiosInstance.js
@@ -1,14 +1,21 @@
-// src/api/axiosInstance.js
 import axios from "axios";
-
-const token = localStorage.getItem("token");
 
 export const authInstance = axios.create({
   baseURL: `https://${import.meta.env.VITE_API_BASE_URL}`, // 기본 URL 설정
-  headers: {
-    Authorization: token ? `Bearer ${token}` : "",
-  },
 });
+
+authInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export const baseInstance = axios.create({
   baseURL: `https://${import.meta.env.VITE_API_BASE_URL}`, // 기본 URL 설정
