8ac784d296c7b50152b813c2047d545c605bd8fd
yonghyeonpark <pyh4616@naver.com>
2024-06-24 21:43:27 +0900

    refactor: 불필요한 uri 제거

--- src/main/java/codesquad/team05/web/like/LikeController.java
+++ src/main/java/codesquad/team05/web/like/LikeController.java
@@ -7,9 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URI;
 
 @RestController
 @RequestMapping("/likes")
@@ -21,15 +18,11 @@ public class LikeController {
     @PostMapping("/{userId}/{accommodationId}")
     public ResponseEntity<Long> like(
             @PathVariable Long userId,
-            @PathVariable Long accommodationId,
-            UriComponentsBuilder uriComponentsBuilder
+            @PathVariable Long accommodationId
     ) {
         Long likeId = likeService.like(userId, accommodationId);
-        URI location = uriComponentsBuilder.path("/likes/{id}")
-                .buildAndExpand(likeId)
-                .toUri();
         return ResponseEntity
-                .created(location)
-                .build();
+                .ok()
+                .body(likeId);
     }
 }
