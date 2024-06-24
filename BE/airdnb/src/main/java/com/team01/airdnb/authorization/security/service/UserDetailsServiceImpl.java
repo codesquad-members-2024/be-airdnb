3cd738106c7d65873e308d87ab5212f5c0f69e6e
mir <ogt124321@gmail.com>
2024-06-24 15:28:04 +0900

    refactor: 유저 repository에 직접 접근하지 않고 서비스를 통해 데이터를 주고받도록 변경 #26

--- BE/airdnb/src/main/java/com/team01/airdnb/authorization/security/service/UserDetailsServiceImpl.java
+++ BE/airdnb/src/main/java/com/team01/airdnb/authorization/security/service/UserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package com.team01.airdnb.authorization.security.service;
 
 import com.team01.airdnb.user.PrincipalDetail;
 import com.team01.airdnb.user.UserRepository;
+import com.team01.airdnb.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -17,14 +18,14 @@ import java.util.Collections;
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.info("--------------------------- UserDetailsServiceImpl ---------------------------");
         log.info("username = {}", username);
 
-        return userRepository.findByEmail(username)
+        return userService.findByEmail(username)
                 .map(member -> new PrincipalDetail(member, Collections.singleton(new SimpleGrantedAuthority(member.getRole().getValue()))))
                 .orElseThrow(() -> new UsernameNotFoundException("등록되지 않은 사용자입니다"));
     }
