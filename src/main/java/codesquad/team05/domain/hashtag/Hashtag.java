b4e231846092f17e75a0c3dda15efe0463c31a6d
yonghyeonpark <pyh4616@naver.com>
2024-06-24 19:47:19 +0900

    refactor; 생성자에 양방향 매핑을 위한 연관관계 로직 추가

--- src/main/java/codesquad/team05/domain/hashtag/Hashtag.java
+++ src/main/java/codesquad/team05/domain/hashtag/Hashtag.java
@@ -2,8 +2,11 @@ package codesquad.team05.domain.hashtag;
 
 import codesquad.team05.domain.accommodation.Accommodation;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Hashtag {
 
     @Id
@@ -14,4 +17,13 @@ public class Hashtag {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "accommodation_id", nullable = false)
     private Accommodation accommodation;
+
+    public Hashtag(String content, Accommodation accommodation) {
+        this.content = content;
+        if (accommodation != null) {
+            accommodation.getHashtags().remove(this);
+        }
+        this.accommodation = accommodation;
+        accommodation.getHashtags().add(this);
+    }
 }
