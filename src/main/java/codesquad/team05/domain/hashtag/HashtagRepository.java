4c615284d0f837efa9ca53c92d544fca4e01f6f7
yonghyeonpark <pyh4616@naver.com>
2024-06-24 20:08:22 +0900

    fix: 올바른 엔터티 클래스를 지정

--- src/main/java/codesquad/team05/domain/hashtag/HashtagRepository.java
+++ src/main/java/codesquad/team05/domain/hashtag/HashtagRepository.java
@@ -1,8 +1,6 @@
 package codesquad.team05.domain.hashtag;
 
-import codesquad.team05.domain.hastag.Hastag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HashtagRepository extends JpaRepository<Hastag, Long> {
-
+public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
 }
