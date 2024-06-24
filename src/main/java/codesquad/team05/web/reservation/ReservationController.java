4dca927c511f4bdfd082393219521601b35b337e
yonghyeonpark <pyh4616@naver.com>
2024-06-24 16:32:17 +0900

    refactor: 불필요한 클래스 삭제 및 반영

--- src/main/java/codesquad/team05/web/reservation/ReservationController.java
+++ src/main/java/codesquad/team05/web/reservation/ReservationController.java
@@ -1,7 +1,6 @@
 package codesquad.team05.web.reservation;
 
 import codesquad.team05.service.ReservationService;
-import codesquad.team05.service.reservation.ReservationBusinessService;
 import codesquad.team05.web.reservation.dto.request.ReservationSave;
 import codesquad.team05.web.reservation.dto.response.ReservationResponse;
 import lombok.RequiredArgsConstructor;
@@ -21,7 +20,6 @@ public class ReservationController {
     // to-do
     // User 기능 완성하면 @AuthenticationPrincipal PrincipalDetails userDetails을 활용해서 세션으로 userID집어넣기
     private final ReservationService reservationService;
-    private final ReservationBusinessService reservationBusinessService;
 
     @PostMapping("/{id}")
     public ResponseEntity<Long> reserve(
@@ -29,7 +27,7 @@ public class ReservationController {
             @RequestBody ReservationSave requestForm,
             UriComponentsBuilder uriComponentsBuilder
     ) {
-        Long reservationId = reservationBusinessService.reserveAccommodation(1L, id, requestForm.toServiceDto());
+        Long reservationId = reservationService.createReservation(1L, id, requestForm.toServiceDto());
         URI location = uriComponentsBuilder.path("/reservations/{id}")
                 .buildAndExpand(reservationId)
                 .toUri();
