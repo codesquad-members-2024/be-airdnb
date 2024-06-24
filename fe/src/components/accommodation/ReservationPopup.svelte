09c55f4c5d3c8d09ef9012716d350c6bc9226088
Zoonmy <pjm2571@naver.com>
2024-06-24 17:12:49 +0900

    refactor: Reservation 전송 시, jwt 토큰 이름을 accessToken으로 수정

--- fe/src/components/accommodation/ReservationPopup.svelte
+++ fe/src/components/accommodation/ReservationPopup.svelte
@@ -10,7 +10,7 @@
   const dispatch = createEventDispatcher();
 
   function getAuthToken() {
-    return localStorage.getItem('jwt');
+    return localStorage.getItem('accessToken');
   }
 
   function closePopup() {
