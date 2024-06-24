72d6faca4374d32005e222788699a77711426ebf
choejieun <wldms5221@icloud.com>
2024-06-24 14:15:12 +0900

    refactor: count -> exist 수정 및 필터링 추가

--- airdnb/src/main/java/team10/airdnb/reservation/repository/ReservationRepositoryImpl.java
+++ airdnb/src/main/java/team10/airdnb/reservation/repository/ReservationRepositoryImpl.java
@@ -5,9 +5,10 @@ import jakarta.persistence.LockModeType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
-import team10.airdnb.reservation.entity.QReservation;
 
 import java.time.LocalDate;
+
+import static team10.airdnb.reservation.entity.QReservation.*;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,21 +19,25 @@ public class ReservationRepositoryImpl implements ReservationRepositoryCustom {
     @Override
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     public boolean isDateRangeAvailable(Long accommodationId, LocalDate checkInDate, LocalDate checkOutDate) {
-        QReservation reservation = QReservation.reservation;
 
-        long count = queryFactory.selectFrom(reservation)
+        boolean exists = queryFactory.selectFrom(reservation)
                 .where(
                         reservation.accommodation.id.eq(accommodationId)
                                 .and(
-                                        reservation.checkInDate.lt(checkOutDate)
-                                                .and(reservation.checkOutDate.gt(checkInDate))
-                                                .or(reservation.checkInDate.eq(checkOutDate)) //체크인과 체크아웃 날짜가 같은 것을 허용
-                                                .or(reservation.checkOutDate.eq(checkInDate))
+                                        reservation.checkInDate.loe(checkOutDate)
+                                                .and(reservation.checkOutDate.goe(checkInDate))
+                                )
+                                .and(
+                                        reservation.checkInDate.ne(checkOutDate)
+                                )
+                                .and(
+                                        reservation.checkOutDate.ne(checkInDate)
                                 )
                 )
-                .fetchCount();
+                .fetchCount() > 0;
 
-        return count == 0;
+        return !exists;
+
     }
 }
 
