53642b1ed38ead7a8b9f8d4e046212cfb6524440
mir <ogt124321@gmail.com>
2024-06-25 13:48:32 +0900

    feat: 호스트가 가지고 있는 숙소들의 예약 정보를 확인 할수있는 리스트를 반환하는 기능 구현 #26

--- BE/airdnb/src/main/java/com/team01/airdnb/reservation/ReservationService.java
+++ BE/airdnb/src/main/java/com/team01/airdnb/reservation/ReservationService.java
@@ -2,12 +2,19 @@ package com.team01.airdnb.reservation;
 
 import com.team01.airdnb.accommadation.Accommodation;
 import com.team01.airdnb.accommadation.AccommodationService;
+import com.team01.airdnb.accommadation.dto.AccommodationHostResponse;
+import com.team01.airdnb.reservation.dto.ReservationHostListResponse;
 import com.team01.airdnb.reservation.dto.ReservationRequest;
 import com.team01.airdnb.reservation.dto.ReservationShowResponse;
 import com.team01.airdnb.user.User;
 import com.team01.airdnb.user.UserService;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,6 +32,11 @@ public class ReservationService {
     this.reservationRepository = reservationRepository;
     this.userService = userService;
     this.accommodationService = accommodationService;
+  }
+
+  private Reservation findById(Long reservationId) {
+    return reservationRepository.findById(reservationId)
+        .orElseThrow(() -> new NoSuchElementException("존재하지 않는 예약입니다."));
   }
 
   public void createReservation(ReservationRequest reservationRequest) {
@@ -55,8 +67,7 @@ public class ReservationService {
 
   public void updateReservation(Long reservationId,
       ReservationRequest reservationRequest) {
-    Reservation reservation = reservationRepository.findById(reservationId)
-        .orElseThrow(() -> new NoSuchElementException("존재하지 않는 예약입니다."));
+    Reservation reservation = findById(reservationId);
 
     if (checkAvailability(reservationRequest)) {
       throw new IllegalArgumentException("예약이 불가능합니다.");
@@ -67,9 +78,12 @@ public class ReservationService {
 
   @Transactional(readOnly = true)
   public ReservationShowResponse showReservation(Long reservationId) {
-    Reservation reservation = reservationRepository.findById(reservationId)
-        .orElseThrow(() -> new NoSuchElementException("존재하지 않는 예약입니다."));
+    Reservation reservation = findById(reservationId);
 
+    return toReservationShowResponse(reservation);
+  }
+
+  public ReservationShowResponse toReservationShowResponse(Reservation reservation) {
     return ReservationShowResponse.builder()
         .reservationID(reservation.getId())
         .accommodationID(reservation.getAccommodation().getId())
@@ -82,5 +96,34 @@ public class ReservationService {
         .price(reservation.getPrice())
         .userId(reservation.getUser().getId())
         .build();
+  }
+
+  @Transactional(readOnly = true)
+  public List<ReservationShowResponse> findAll (){
+    List<Reservation> reservations = reservationRepository.findAll();
+    List<ReservationShowResponse> reservationShowResponses = new ArrayList<>();
+
+    for (Reservation reservation : reservations) {
+      reservationShowResponses.add(toReservationShowResponse(reservation));
+    }
+    return reservationShowResponses;
+  }
+
+  @Transactional(readOnly = true)
+  public List<ReservationHostListResponse> getOwnedAccommodationReservation(Authentication authentication) {
+    User user = userService.findByEmail(authentication.getName())
+        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    List<AccommodationHostResponse> accommodations = accommodationService.getAccommodationByUserId(user.getId());
+    List<ReservationHostListResponse> reservationHostListResponses = new ArrayList<>();
+
+    for (AccommodationHostResponse accommodation : accommodations) {
+      List<Reservation> reservations = reservationRepository.findAllByAccommodationIdAndHostId(accommodation.id(), user.getId());
+      List<ReservationShowResponse> reservationShowResponses = reservations.stream()
+          .map(this::toReservationShowResponse)
+          .collect(Collectors.toList());
+      reservationHostListResponses.add(new ReservationHostListResponse(accommodation, reservationShowResponses));
+    }
+
+    return reservationHostListResponses;
   }
 }
