ce7a1f4187c586d21f27a00788715348732ba420
choejieun <wldms5221@icloud.com>
2024-06-25 16:16:50 +0900

    feat: memberId별 예약 된 숙소 가져오는 api 구현

---
+++ airdnb/src/main/java/team10/airdnb/reservation/dto/ReservationAccommodationDto.java
@@ -0,0 +1,22 @@
+package team10.airdnb.reservation.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record ReservationAccommodationDto(
+        Long reservationId,
+        LocalDate checkInDate,
+        LocalDate checkOutDate,
+        Integer capacity,
+        BigDecimal totalPrice,
+        String accommodationName,
+        String accommodationDescription,
+        String accommodationImages,
+        String city,
+        String district,
+        String neighborhood,
+        String streetName,
+        String detailedAddress
+) {
+
+}
--- airdnb/src/main/java/team10/airdnb/reservation/repository/ReservationRepositoryImpl.java
+++ airdnb/src/main/java/team10/airdnb/reservation/repository/ReservationRepositoryImpl.java
@@ -1,13 +1,18 @@
 package team10.airdnb.reservation.repository;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.LockModeType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
+import team10.airdnb.accommodation.entity.QAccommodation;
+import team10.airdnb.reservation.dto.ReservationAccommodationDto;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import static team10.airdnb.accommodation.entity.QAccommodation.*;
 import static team10.airdnb.reservation.entity.QReservation.*;
 
 @Repository
@@ -38,6 +43,32 @@ public class ReservationRepositoryImpl implements ReservationRepositoryCustom {
 
         return !exists;
 
+    }
+
+    @Override
+    public List<ReservationAccommodationDto> findReservationAccommodationDTOsByMemberId(String memberId) {
+
+        return queryFactory
+                .select(Projections.constructor(
+                        ReservationAccommodationDto.class,
+                        reservation.id.as("reservationId"),
+                        reservation.checkInDate,
+                        reservation.checkOutDate,
+                        reservation.capacity,
+                        reservation.totalPrice,
+                        accommodation.name.as("accommodationName"),
+                        accommodation.accommodationDescription.as("accommodationDescription"),
+                        accommodation.accommodationImages.as("accommodationImages"),
+                        accommodation.address.city.as("city"),
+                        accommodation.address.district.as("district"),
+                        accommodation.address.neighborhood.as("neighborhood"),
+                        accommodation.address.streetName.as("streetName"),
+                        accommodation.address.detailedAddress.as("detailedAddress")
+                ))
+                .from(reservation)
+                .join(reservation.accommodation, accommodation)
+                .where(reservation.member.id.eq(memberId))
+                .fetch();
     }
 }
 
