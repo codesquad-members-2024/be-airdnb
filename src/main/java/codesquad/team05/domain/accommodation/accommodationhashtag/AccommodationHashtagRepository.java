466db566157d688757d3d67f870f12f90c468489
yonghyeonpark <pyh4616@naver.com>
2024-06-26 22:13:52 +0900

    feat: 중간 테이블 Repository 구현

---
+++ src/main/java/codesquad/team05/domain/accommodation/accommodationhashtag/AccommodationHashtagRepository.java
@@ -0,0 +1,6 @@
+package codesquad.team05.domain.accommodation.accommodationhashtag;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccommodationHashtagRepository extends JpaRepository<AccommodationHashtag, Long> {
+}
