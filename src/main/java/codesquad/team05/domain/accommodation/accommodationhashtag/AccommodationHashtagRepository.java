0ea0c4c4968e93b80e356aabad3e528ec585bc8a
yonghyeonpark <pyh4616@naver.com>
2024-06-28 11:10:51 +0900

    feat: 복합키를 기준으로 존재 여부 판단 및 조회하는 기능 구현

--- src/main/java/codesquad/team05/domain/accommodation/accommodationhashtag/AccommodationHashtagRepository.java
+++ src/main/java/codesquad/team05/domain/accommodation/accommodationhashtag/AccommodationHashtagRepository.java
@@ -2,5 +2,11 @@ package codesquad.team05.domain.accommodation.accommodationhashtag;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccommodationHashtagRepository extends JpaRepository<AccommodationHashtag, Long> {
+
+    boolean existsByAccommodationHashtagId(AccommodationHashtagId accommodationHashtagId);
+
+    Optional<AccommodationHashtag> findByAccommodationHashtagId(AccommodationHashtagId accommodationHashtagId);
 }
