0af33fb7976dd8c5bf15795c9c87869e9b5ca110
seondays <agseon9@gmail.com>
2024-06-28 16:37:38 +0900

    fix: 주석 오류 제거

--- BE/airdnb/src/main/java/com/team01/airdnb/authorization/security/service/UserDetailsServiceImpl.java
+++ BE/airdnb/src/main/java/com/team01/airdnb/authorization/security/service/UserDetailsServiceImpl.java
@@ -1,32 +1,32 @@
-//package com.team01.airdnb.authorization.security.service;
-//
-//import com.team01.airdnb.user.PrincipalDetail;
-//import com.team01.airdnb.user.UserRepository;
-//import com.team01.airdnb.user.UserService;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.Collections;
-//
-//@Slf4j
-//@Service
-//@RequiredArgsConstructor
-//public class UserDetailsServiceImpl implements UserDetailsService {
-//
-//    private final UserService userService;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        log.info("--------------------------- UserDetailsServiceImpl ---------------------------");
-//        log.info("username = {}", username);
-//
-//        return userService.findByEmail(username)
-//                .map(member -> new PrincipalDetail(member, Collections.singleton(new SimpleGrantedAuthority(member.getRole().getValue()))))
-//                .orElseThrow(() -> new UsernameNotFoundException("등록되지 않은 사용자입니다"));
-//    }
-//}
+package com.team01.airdnb.authorization.security.service;
+
+import com.team01.airdnb.user.PrincipalDetail;
+import com.team01.airdnb.user.UserRepository;
+import com.team01.airdnb.user.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("--------------------------- UserDetailsServiceImpl ---------------------------");
+        log.info("username = {}", username);
+
+        return userService.findByEmail(username)
+                .map(member -> new PrincipalDetail(member, Collections.singleton(new SimpleGrantedAuthority(member.getRole().getValue()))))
+                .orElseThrow(() -> new UsernameNotFoundException("등록되지 않은 사용자입니다"));
+    }
+}
